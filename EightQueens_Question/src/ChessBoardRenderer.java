09125c6c0d54e97e0df5d0743f7b79a9f4fc9b3e
AkshayB2017 <akbgunner4ever@gmail.com>
2020-07-09 20:33:30 +0530

    Solved the 8 Queens problem

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -1,8 +1,18 @@
 public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
-
-		// WRITE YOUR LOGIC HERE...
+        int counter=0,flag=0; int number = 1; 
 		System.out.println(square);
-		return true;
+        
+        for(int i=0;i<8;i++)
+        {
+        	for(int j=0;j<8;j++) {
+        		if((i+(8*j))==square && ((i%2==0)==(j%2==0)))
+        		{
+        			return true;
+        		}
+        	}
+        }
+        return false;
+        
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -5,14 +5,47 @@ public class GameStrategy {
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
 
-		return 0;	// just for the heck of it
+		int col= 0;
+		if(cellId%8==0)
+			col=0;
+		else if((cellId-1)%8==0)
+			col=1;
+		else if((cellId-2)%8==0)
+			col=2;
+		else if((cellId-3)%8==0)
+			col=3;
+		else if((cellId-4)%8==0)
+			col=4;
+		else if((cellId-5)%8==0)
+			col=5;
+		else if((cellId-6)%8==0)
+			col=6;
+		else
+			col=7;
+		return col;	
 	}
 	
 	private int getRow(int cellId) {
-		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		int row = 0;
+		if(cellId<8)
+			row=0;
+		else if(cellId>7&&cellId<16)
+			row=1;
+		else if(cellId>15&&cellId<24)
+			row=2;
+		else if(cellId>23&&cellId<32)
+			row=3;
+		else if(cellId>31&&cellId<40)
+			row=4;
+		else if(cellId>39&&cellId<48)
+			row=5;
+		else if(cellId>47&&cellId<56)
+			row=6;
+		else if(cellId>55&&cellId<64)
+			row=7;
+		return row;
 	}
+
 
 	public boolean isValidPosition(int cellId) {
 		boolean isValid = true;
@@ -24,14 +57,41 @@ public class GameStrategy {
 		int col = getColumn(cellId);
 		
 		int row = getRow(cellId);
-		
-		/*
-			WRITE YOUR LOGIC HERE...............................
+		if(placedQueens[row][col] == false) {
+            placedQueens[row][col] = true;
+            for(int i=0; i<8; i++) {
+                placedQueens[row][i] = true;
+                placedQueens[i][col] = true;
+            }
+            int i = row;
+            int j = col;
+            while(i<8 && j<8) {
+                placedQueens[i++][j++] = true;
+            }
+            i = row;
+            j = col;
+            while(i>=0 && j>=0) {
+                placedQueens[i--][j--] = true;
+            }
+            i = row;
+            j = col;
+            while(i>=0 && j<8) {
+                placedQueens[i--][j++] = true;
+            }
+            i = row;
+            j = col;
+            while(i<8 && j>=0) {
+                placedQueens[i++][j--] = true;
+            }
 
-		*/
-		return isValid;
-	}
+            return isValid;
+        }
+        else {
+            return !isValid;
+        }
+    }
 }
+
 
 
 
